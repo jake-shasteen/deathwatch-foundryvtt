02d10affef3cce42fbb78974f04faf66ed2f39b6
Jake Shasteen <jakeshasteen@gmail.com>
2024-02-26 01:43:42 -0800

    Get sheet rendering chapter and specialization

--- main.mjs
+++
@@ -1,6 +0,0 @@
-function helloWorld() {
-  console.log("hello, world");
-}
-Hooks.once("init", helloWorld);
-Hooks.once("setup", helloWorld);
-Hooks.once("ready", helloWorld);
--- src/main.ts
+++ src/main.ts
@@ -3,28 +3,65 @@ Hooks.once("init", function init() {
   console.log(`${systemName} | initializing system`);
   CONFIG.Item.documentClass = class SystemItemDocument extends Item {};
   Items.unregisterSheet("core", ItemSheet);
-  Items.registerSheet(systemName, class SystemItemSheet extends ItemSheet {
-    static get defaultOptions() {
-      return foundry.utils.mergeObject(super.defaultOptions, {
-        classes: [systemName, "sheet", "actor"],
-        template: `systems/${systemName}/templates/item-sheet.html`
-      });
-    }
-  }, { makeDefault: true });
-  
-  CONFIG.Actor.documentClass = class SystemActorDocument extends Actor {};
-  Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet(systemName, class SystemActorSheet extends ActorSheet {
-    static get defaultOptions() {
-      return foundry.utils.mergeObject(super.defaultOptions, {
-        classes: [],
-        template: `systems/${systemName}/templates/actor-sheet.html`
-      });
-    }
-  }, { makeDefault: true });
+  Items.registerSheet(
+    systemName,
+    class SystemItemSheet extends ItemSheet {
+      static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+          classes: [systemName, "sheet", "item"],
+          template: `systems/${systemName}/templates/item-sheet.html`,
+        });
+      }
+    },
+    { makeDefault: true }
+  );
 
-  CONFIG.Token.documentClass = class SystemTokenDocument extends TokenDocument {};
+  CONFIG.Actor.documentClass = class SystemActorDocument extends Actor {
+    prepareData() {
+      super.prepareData();
+    }
+
+    prepareBaseData() {}
+
+    prepareDerivedData() {}
+  };
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet(
+    systemName,
+    class SystemActorSheet extends ActorSheet {
+      static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+          classes: [systemName, "sheet", "actor"],
+          template: `systems/${systemName}/templates/actor-sheet.html`,
+        });
+      }
+    },
+    { makeDefault: true }
+  );
+
+  CONFIG.Token.documentClass = class SystemTokenDocument extends (
+    TokenDocument
+  ) {};
   CONFIG.Token.objectClass = class SystemTokenObject extends Token {};
+
+  Handlebars.registerHelper(
+    "each_when",
+    function (list, propName, value, options) {
+      const result = list.filter(
+        (systemItemDocument: Record<string, any>) =>
+          systemItemDocument[propName] == value
+      ).map((systemItemDocument: Record<string, any>) =>
+        options.fn({ item: systemItemDocument })
+      );
+
+      return result.length > 0
+        ? result.reduce(
+            (resultHtml: string, currHtml: string) => (resultHtml += currHtml),
+            ""
+          )
+        : options.inverse();
+    }
+  );
 
   console.log(`${systemName} | finished initialization`);
 });
